eda7431b06cdc49319220a00954dac9082e90e7e
Jake Schneider <jake.w.schneider@gmail.com>
2019-01-15 14:47:23 -0500

    Example - Part 3

--- scripts/main.js
+++ scripts/main.js
@@ -29,7 +29,10 @@ var svg = d3.select("body").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+    .attr("class", "chart")
+    .attr("id", "apple-stock-chart");
+
 
 d3.tsv("data.tsv", function(error, data) {
   if (error) throw error;
